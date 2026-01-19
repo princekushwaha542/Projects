c3d399508c38ef05c6f918173f1da9f94fd1ee00
Prince Kushwaha <kushwahaprince43668@gmail.com>
2026-01-19 17:59:12 +0530

    DRAG ON TASK COLUMS

---
+++ KANBAN BOARD PROJECT/script.js
@@ -0,0 +1,18 @@
+const todo = document.querySelector("#todo");
+const progress = document.querySelector("#progress");
+const done =document.querySelector("#done");
+
+const tasks = document.querySelectorAll(".task");
+
+tasks.forEach(task =>{
+    task.addEventListener("drag",(e) => {
+    console.log("dragging", e);
+    })
+})
+
+progress.addEventListener("dragenter",(e) =>{
+    progress.classList.add("hover-over");
+})
+progress.addEventListener("dragleave",(e) =>{
+    progress.classList.remove("hover-over");
+})
