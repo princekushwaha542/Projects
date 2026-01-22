6ffe6e4df5b1c50f904fdcc528e67c9ad1c9bf1c
Prince Kushwaha <kushwahaprince43668@gmail.com>
2026-01-22 22:29:22 +0530

    ADD TASK TO ACTIVE

--- KANBAN BOARD PROJECT/script.js
+++ KANBAN BOARD PROJECT/script.js
@@ -1,12 +1,15 @@
 const todo = document.querySelector("#todo");
 const progress = document.querySelector("#progress");
 const done =document.querySelector("#done");
+let dragElement = null;
 
 const tasks = document.querySelectorAll(".task");
 
 tasks.forEach(task =>{
     task.addEventListener("drag",(e) => {
     // console.log("dragging", e);
+    dragElement = task;
+
     })
 })
 
@@ -17,9 +20,26 @@ function addDragEventOnColumn(column){
     })
      column.addEventListener("dragleave",(e) => {
         e.preventDefault();
-        column.classList.remove("hover-over")
+        // column.classList.remove("hover-over")
+        console.log("dropped",dragElement,column);
+        column.classList.remove("hover-over");
+        column.appendChild(dragElement);
     })
 }
 addDragEventOnColumn(todo);
 addDragEventOnColumn(progress);
 addDragEventOnColumn(done);
+
+
+const toggleModalBtn = document.querySelector("#toggle-modal");
+const modal = document.querySelector(".modal");
+const modalBg = document.querySelector(".modal .bg");
+
+toggleModalBtn.addEventListener("click",() =>{
+    modal.classList.toggle("active");
+
+})
+
+modalBg.addEventListener("click", ()=>{
+    modal.classList.remove("active");
+})
